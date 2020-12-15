e2b5f3bb4d1263aa9d583239b47d1ae31f75e275
Kata Szasz <Kata@invevo.com>
2020-12-15 07:36:48 +0000

    solve advanced-react-patterns 01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -77,6 +77,19 @@ function useUser() {
 // Then go down to the `handleSubmit` from `UserSettings` and put that logic in
 // this function. It should accept: dispatch, user, and updates
 
+
+async function updatedUser(dispatch, user, updates) {
+  dispatch({type: 'start update', updates})
+  try {
+    const updatedUser = await userClient.updateUser(user, updates)
+    dispatch({type: 'finish update', updatedUser})
+    return updatedUser
+  } catch (error) {
+    dispatch({type: 'fail update', error})
+    return Promise.reject(error)
+  }
+}
+
 // export {UserProvider, useUser}
 
 // src/screens/user-profile.js
@@ -97,12 +110,7 @@ function UserSettings() {
 
   function handleSubmit(event) {
     event.preventDefault()
-    // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+    updatedUser(userDispatch, user, formState).catch(() => {})  
   }
 
   return (
