053eee9be1d2a46021d3a978e876382c19ae03a7
Kata Szasz <Kata@invevo.com>
2021-01-13 07:14:05 +0000

    solve advanced-react-patterns 06 - extra2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -47,6 +47,18 @@ function useToggle({
     )
   }, [hasOnChange, onIsControlled, readOnly])
 
+  const {current: onWasControlled} = React.useRef(onIsControlled)
+  React.useEffect(() => {
+    warning(
+      !(onIsControlled && !onWasControlled),
+      `\`useToggle\` is changing from uncontrolled to be controlled. Components should not switch from uncontrolled to controlled (or vice versa). Decide between using a controlled or uncontrolled \`useToggle\` for the lifetime of the component. Check the \`on\` prop.`,
+    )
+    warning(
+      !(!onIsControlled && onWasControlled),
+      `\`useToggle\` is changing from controlled to be uncontrolled. Components should not switch from controlled to uncontrolled (or vice versa). Decide between using a controlled or uncontrolled \`useToggle\` for the lifetime of the component. Check the \`on\` prop.`,
+    )
+  }, [onIsControlled, onWasControlled])
+
   function dispatchWithOnChange(action) {
     if (!onIsControlled) {
       dispatch(action)
@@ -88,7 +100,7 @@ function Toggle({on: controlledOn, onChange, readOnly}) {
 }
 
 function App() {
-  const [bothOn, setBothOn] = React.useState(false)
+  const [bothOn, setBothOn] = React.useState()
   const [timesClicked, setTimesClicked] = React.useState(0)
 
   function handleToggleChange(state, action) {
